1c97385dea213c9ecfff65c2ef86c3630d85cd6d
Ajeet singh <singhajeet335@gmail.com>
2022-12-29 13:36:36 +0530

    bash codes

---
+++ BashCodes/Day_1/oddPrint.bash
@@ -0,0 +1,7 @@
+for((i=1;i<=99;i++))
+ do   
+    if [ $(($i%2)) -ne 0 ]
+    then
+        echo "$i"
+    fi
+done
---
+++ BashCodes/Day_1/substrac.sh
@@ -0,0 +1,6 @@
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+ 
+result=$(( $num1 - $num2 ))
+ 
+echo "Result is: $result"  
